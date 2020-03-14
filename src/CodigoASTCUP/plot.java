ea1f9f9f97ec29fb6d760139c836b69e14fd2fbe
Romeo <misinicialesbray@gmail.com>
2020-03-14 13:35:33 -0600

    plot DISPERSION

--- src/CodigoASTCUP/plot.java
+++ src/CodigoASTCUP/plot.java
@@ -72,17 +72,54 @@ public class plot extends NodoAbstracto{
             f.setVisible(true);
               
           }else{
-            salida.append("#ERROR: ERRORE EN UN PARAMETRO PLOT\n");
-            TError ERRORES = new TError("histograma",this.linea,this.columna,"Semantico", "ERRORE EN UN PARAMETRO PLOT"  );
-            TABLA_DE_ERRORES_SINTACTICOS.add(ERRORES);
-            return "#Error";
+              System.out.println("VAMS A PLOETANR ALGO QUE NO ES O");
+             System.out.println("VAMOS A PLOTEAR UNA MATRIZ");
+                //POR EL MOMENTO SOLO UN VECTOR xd
+                //ACÁ IRIA EL PLOTEO xd
+                XYSeries series = new XYSeries(TipoGrafica);
+                this.Expresiones = entorno.ObtenerLista(VectorNumeros);
+                this.Expresiones2 = entorno.ObtenerLista(TituloGrafica);
+                double limiteInicial = 0;//Double.parseDouble(this.Expresiones2.get(0).Ejecutar(entorno, salida));
+                double limiteFinal = 0;
+                if(this.Expresiones2.size() ==2){
+                    limiteInicial = Double.parseDouble(this.Expresiones2.get(0).Ejecutar(entorno, salida));
+                    limiteFinal = Double.parseDouble(this.Expresiones2.get(1).Ejecutar(entorno, salida));
+                }else{
+                    salida.append("#ERROR: ERRORE EN UN PARAMETRO PLOT\n");
+                    TError ERRORES = new TError("histograma",this.linea,this.columna,"Semantico", "ERRORE EN UN PARAMETRO PLOT"  );
+                    TABLA_DE_ERRORES_SINTACTICOS.add(ERRORES);
+                    
+                    return "#Error";
+                }
+                
+                
+                for(int i = 0; i < this.Expresiones.size(); i++){
+                    double numerito = Double.parseDouble(this.Expresiones.get(i).Ejecutar(entorno, salida));
+                    if(numerito > limiteInicial && numerito < limiteFinal){
+                        series.add(i+1,numerito);
+                    }
+                    
+                }
+                XYSeriesCollection dataset = new XYSeriesCollection();
+                dataset.addSeries(series);
+                JFreeChart  chart = ChartFactory.createScatterPlot(TipoGrafica, Nombrex, NombreY, dataset, PlotOrientation.VERTICAL, true, true, true);
+                ChartFrame f = new ChartFrame("Titulo",chart);
+            f.setSize(1000,600);
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+          
           }
           
         }else{
-            salida.append("#ERROR: ERRORE EN UN PARAMETRO PLOT\n");
-            TError ERRORES = new TError("histograma",this.linea,this.columna,"Semantico", "ERRORE EN UN PARAMETRO PLOT"  );
-            TABLA_DE_ERRORES_SINTACTICOS.add(ERRORES);
-            return "#Error";
+           
+                 salida.append("#ERROR: ERRORE EN UN PARAMETRO PLOT\n");
+                    TError ERRORES = new TError("histograma",this.linea,this.columna,"Semantico", "ERRORE EN UN PARAMETRO PLOT"  );
+                    TABLA_DE_ERRORES_SINTACTICOS.add(ERRORES);
+                    
+                    return "#Error";
+           
+                
+           
             
         }
         return "FIN PLOT";
